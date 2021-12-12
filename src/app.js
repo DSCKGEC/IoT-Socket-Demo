84ec72e8afdd5dfe2ee5b1106251afe9ec324b0c
Anindya Mitra <55695557+akm15machine@users.noreply.github.com>
2021-12-12 20:37:33 +0530

    refactored

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const io = require("socket.io");
+const socketio = require("socket.io");
 const { instrument } = require("@socket.io/admin-ui");
 const cors = require("cors");
-const path = require('path');
+const path = require("path");
 
 // Globals
 const PORT = Number(process.env.PORT || 3000);
@@ -12,7 +12,7 @@ const SOCK_PORT = Number(process.env.SOCK_PORT || 1234);
 
 // Config for express app
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, './views'))
+app.set("views", path.join(__dirname, "./views")); //set path for the views
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,37 +22,47 @@ app.get("/", async (req, res) => {
   //   res.send(`${new Date()} | Server running successfully!`);
   res.render("pages/index");
 });
+// 404 page
 app.get("*", async (req, res) => {
   res.status(404).send("404 not found");
 });
 
 // Listener
 try {
-  //socket server
-  const socket = io(SOCK_PORT, {
-    cors: {
-      origin: "*",
-    },
-  });
-  socket.on("connection", (sockets) => {
-    console.log(`user connected`);
-    sockets.on("disconnect", () => {
-      console.log(`user disconnected`);
-    });
-    sockets.on("toggle", (data) => {
-      console.log(data);
-    });
-    sockets.on("slider", (data) => {
-      console.log(data);
-    });
-  });
-
   //http server
   app.listen(PORT, () => {
     console.log(`Listening at port: ${PORT}`);
   });
+  //socket server
+  const io = socketio(SOCK_PORT, {
+    cors: {
+      origin: "*",
+    },
+  });
+  io.on("connection", (socket) => {
+    console.log(`User with id: ${socket.id} connected!`);
+    socket.on("disconnect", () => {
+      console.log(`User with id: ${socket.id} disconnected`);
+    });
+    socket.on("toggle", (data) => {
+      console.log(data);
+      socket.broadcast.emit("toggle", data);
+    });
+    socket.on("slider", (data) => {
+      console.log(data);
+      socket.broadcast.emit("slider", data);
+    });
+    // var i = false;
+    // setInterval(()=>{
+    //   i = !i;
+    //   console.log(i);
+    //   socket.emit("toggle", i);
+    // }, 200);
+  });
+
+
   //admin-ui
-  instrument(socket, { auth: false });
+  instrument(io, { auth: false });
 } catch (err) {
   console.log(`Could not start the server, ${err}`);
 }
