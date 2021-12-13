de1e9c6344389f8aa101fc5e0ae5ba1c87eb02e7
Anindya Mitra <55695557+akm15machine@users.noreply.github.com>
2021-12-13 17:57:48 +0530

    half baked, event listeners working

--- src/views/js/script.js
+++ src/views/js/script.js
@@ -1,7 +1,28 @@
 const port = 4000;
+let Loaded = false;
+let isDark = localStorage.getItem("isDark") == "true" ? true : false;
 var socket = io(`http://localhost:${port}`);
 console.log(socket);
 
-// var slider = document.getElementById("Sliding");
-// var button = document.getElementById("toggle");
-
+window.onload = () => {
+  const themeSwitch = document.getElementById("themeSwitch");
+  const toggle = document.getElementById("toggle");
+  const slider = document.getElementById("slider");
+  // local theme loader script
+  themeSwitch.checked = isDark;
+  document.body.classList.toggle("dark", isDark);
+  //theme change event listener
+  themeSwitch.addEventListener("change", () => {
+    document.body.classList.toggle("dark");
+    localStorage.setItem("isDark", themeSwitch.checked);
+  });
+  //control toggle event listener
+  toggle.addEventListener("change", () => {
+    socket.emit("toggle", toggle.checked);
+  });
+  //control slider event listener
+  slider.addEventListener("input", () => {
+    socket.emit("slider", slider.value);
+  });
+  Loaded = true;
+};
