92915b153efe4558b8cc5f47d0237cbed79ece78
Anindya Mitra <55695557+akm15machine@users.noreply.github.com>
2021-12-13 23:46:49 +0530

    added theme toggle

--- src/views/js/script.js
+++ src/views/js/script.js
@@ -1,6 +1,6 @@
 const port = 4000;
 let Loaded = false;
-let isDark = localStorage.getItem("isDark") == "true" ? true : false;
+let isLight = localStorage.getItem("isLight") == "true" ? true : false;
 var socket = io(`http://localhost:${port}`);
 console.log(socket);
 
@@ -8,16 +8,22 @@ window.onload = () => {
   const themeSwitch = document.getElementById("themeSwitch");
   const toggle = document.getElementById("toggle");
   const slider = document.getElementById("slider");
+  const brightness = document.getElementById("brightness");
   // local theme loader script
-  themeSwitch.checked = isDark;
-  document.body.classList.toggle("dark", isDark);
+  themeSwitch.checked = isLight;
+  document.body.classList.toggle("dark", !isLight);
   //theme change event listener
   themeSwitch.addEventListener("change", () => {
     document.body.classList.toggle("dark");
-    localStorage.setItem("isDark", themeSwitch.checked);
+    localStorage.setItem("isLight", themeSwitch.checked);
   });
-  socket.on("toggle", data => { toggle.checked = data });
-  socket.on("slider", data => { slider.value = data });
+  socket.on("toggle", (data) => {
+    toggle.checked = data;
+  });
+  socket.on("slider", (data) => {
+    slider.value = data;
+    updateBrightness(brightness, data);
+  });
   //control toggle event listener
   toggle.addEventListener("change", () => {
     socket.emit("toggle", toggle.checked);
@@ -25,6 +31,15 @@ window.onload = () => {
   //control slider event listener
   slider.addEventListener("input", () => {
     socket.emit("slider", slider.value);
+    updateBrightness(brightness, slider.value);
   });
   Loaded = true;
 };
+
+const updateBrightness = (doc, data) => {
+  doc.innerText = String(mathMap(data, 0, 1023, 0, 100).toFixed(0)) + "%";
+};
+
+const mathMap = (input, inmin, inmax, outmin, outmax) => {
+  return (input - inmin)*(outmax - outmin)/(inmax - inmin);
+}
